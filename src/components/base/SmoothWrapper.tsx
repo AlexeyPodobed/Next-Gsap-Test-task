09d305235c973c6a70bf6b2fac8284204506cc8a
AlexeyPodobed <alexeyandmail@gmail.com>
2025-06-25 15:01:10 +0300

    Revert "Smooth scroller"
    This reverts commit 30f850809cb82a5cb4f29ab498d0a34d93219037.

--- src/components/base/SmoothWrapper.tsx
+++ src/components/base/SmoothWrapper.tsx
@@ -9,28 +9,22 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 export default function SmoothWrapperInit() {
   useLayoutEffect(() => {
-    const init = () => {
-      const wrapper = document.querySelector<HTMLElement>("#smooth-wrapper");
-      const content = document.querySelector<HTMLElement>("#smooth-content");
+    const wrapper = document.querySelector<HTMLElement>("#smooth-wrapper");
+    const content = document.querySelector<HTMLElement>("#smooth-content");
 
-      if (!wrapper || !content) return;
+    if (!wrapper || !content) return;
 
-      const smoother = ScrollSmoother.create({
-        wrapper,
-        content,
-        smooth: 1.2,
-        effects: true,
-        smoothTouch: 0.1,
-      });
+    const smoother = ScrollSmoother.create({
+      wrapper,
+      content,
+      smooth: 1.2,
+      effects: true,
+      smoothTouch: 0.1,
+    });
 
-      return () => {
-        smoother.kill();
-      };
+    return () => {
+      smoother.kill();
     };
-
-    const id = requestAnimationFrame(init);
-
-    return () => cancelAnimationFrame(id);
   }, []);
 
   return null;
